cb462140044f0974ca21dcff403c101da140e919
ajaajahmad <ajaaj75@gmail.com>
2026-02-05 10:19:25 +0530

    initial commit

---
+++ basics/datatype.js
@@ -0,0 +1,5 @@
+let number = 10; // Number
+let message = "Hi!" // String
+let isHappy = true // Boolean
+let person = { name: "Ajaaj", age: "25" }; // Object
+let colors = ["red", "blue", "green"]; // Array
