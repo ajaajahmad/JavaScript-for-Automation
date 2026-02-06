0e97c3adaf879103564309a1101985fed2f6b655
ajaajahmad <ajaaj75@gmail.com>
2026-02-06 10:49:54 +0530

    initial commit

---
+++ basics/operators.js
@@ -0,0 +1,22 @@
+// Arithmetic Operators (for math)
+
+// + (add)
+// - (subtract)
+// * (multiply)
+// / (devide)
+// % (reminder)
+
+let sum = 5+3; // 8
+let product = 4*2 // 8
+
+// Comparison Operators (to compare values)
+
+// == (equal)
+// != (not equal)
+// > (greater than)
+// < (less than)
+// >= (greater than or eqaul)
+// <= (less than or equal)
+
+console.log(10>5); // true
+console.log(10 == 5); // false
